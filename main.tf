fe516cd32436c5283eb0076eaf81c6ed0027560b
HymaRupa <hymarupa.alluri@gmail.com>
2024-06-05 10:45:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,6 @@ resource "aws_instance" "webserver-ec2" {
       ami           = "ami-0e4fd655fb4e26c30"
       instance_type = "t2.micro"
        tags = {
-         Name = "web_server-ec2"
+         Name = "web_server"
        }
 }  
