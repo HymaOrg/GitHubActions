8a5f46bbefae3c9f3384e23b4f52b9182f1e8f8d
HymaRupa <hymarupa.alluri@gmail.com>
2024-06-05 10:12:21 +0530

    ec2-create

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "aws_instance" "webserver-ec2" {
+      ami           = "ami-0e4fd655fb4e26c30"
+      instance_type = "t2.micro"
+       tags = {
+         Name = "web_server-ec2"
+       }
+}  
